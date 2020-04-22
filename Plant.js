00b04289650b459cb71f63364df4bd36649cef1d
klosinm <monicaklosin@gmail.com>
2020-04-21 20:10:02 -0400

    added more elements to plant data

--- Plant.js
+++ Plant.js
@@ -11,7 +11,7 @@ class Plant {
             this.Watering = info.Watering;
             this.Sunlight = info.Sunlight;
             this.Picture = info.Picture;
-            this.Link = info.Link;
+            
         }
        
     }
@@ -40,35 +40,33 @@ Plant.plantCollection = [];
 
 Plant.create({
     Title: "ZZ Plant",
-    Description: "A very easy to care plant with striking foliage.",
+    Description: "A very easy to care plant with striking foliage. It's stems cann grow up to 3ft indoors. Will grow slowly, so be patient with this one!",
     Watering: "Water when the top two inches of soil are dry - do not let this plant sit in very wet soil ",
     Sunlight: "Can tolerate both bright and low light levels - but it does not like direct sun",
-    Picture: "Images/zzPlant.jpg",
-    Link: "http://www.costafarms.com/plants/stromanthe"
+    Picture: "Images/zzPlant.jpg"
 });
 Plant.create({
     Title: "Monstera Adansonii",
     Description: "Monstera means \"abnormal\" in latin - and leaves on this plant are definitly unique. The leaves develope cute little holes. ",
     Watering: "It likes humidity - so feel free to spritz it with a mister. Water when the top inch of the soil is dry - do not let it sit in soggy soil ",
     Sunlight: "Likes indirect light - but can also grow in lower light conditions",
-    Picture: "Images/monstera.jpg",
-    Link: "http://www.costafarms.com/plants/stromanthe"
+    Picture: "Images/monstera.jpg"
 });
 Plant.create({
     Title: "Stromanthe Trio Star",
     Description: "Called \"Trio\" for the three colors in its leaves - white, pink, green. The brighter the light, the more pink in the leaves. ",
     Watering: "It likes humidity - so feel free to spritz it with a mister. ",
     Sunlight: "Likes bright light, but watch that it does not burn with too much direct light ",
-    Picture: "Images/trioStar.jpg",
-    Link: "http://www.costafarms.com/plants/stromanthe"
+    Picture: "Images/trioStar.jpg"
+   
 });
 Plant.create({
     Title: "Pothos",
-    Description: "Ideal plant for beginners ... ",
+    Description: "Ideal plant for beginners. It's leaves can be golden yellow, as well as green. Will grow in hanging pots, but also anywhere you please. ",
     Watering: "Keep the soil moist but not soggy. If you have not watered in a while and the leaves are drooping it is time to water again.",
     Sunlight: "Can tolerate both bright and low light levels - but it does not like direct sun",
-    Picture: "Images/pothos.jpg",
-    Link: "http://www.costafarms.com/plants/stromanthe"
+    Picture: "Images/pothos.jpg"
+    
 });
 
 Plant.create({
@@ -76,16 +74,15 @@ Plant.create({
     Description: "A beautiful climing plant that has pink, white, and green leaves! It loves light and high humidity. Be careful not to overwater this one. If you take good care of it can bloom pink flowers!",
     Watering: "Wait untill the soil is compleatly dry ~ 1 month. When you water it make sure to fully saturate the soil." ,
     Sunlight: "likes sun",
-    Picture: "Images/hoya.jpg",
-    Link: "http://www.costafarms.com/plants/stromanthe"
+    Picture: "Images/hoya.jpg"
 });
 Plant.create({
     Title: "Christmas Cactus",
     Description: "Though called a cactus - these are not desert natives. They are jungle cacti! That means that they do not like the soil to be bone dry. Take good care of it and it will give you flowers during the winter!",
     Watering: "Water when the top inch of the soil is dry - do not let it sit in soggy soil",
     Sunlight: "They like bright indirect light.",
-    Picture: "Images/christmasCactus.png",
-    Link: "http://www.costafarms.com/plants/stromanthe"
+    Picture: "Images/christmasCactus.png"
+   
 });
 
 
